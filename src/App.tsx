db90562e2788f227795d4e7103fa82920ebcf03d
abouamhster-cmyk <abouamhster@gmail.com>
2026-06-28 21:55:29 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // 📁 src/App.tsx
-
+ 
 import { useEffect, useRef } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
