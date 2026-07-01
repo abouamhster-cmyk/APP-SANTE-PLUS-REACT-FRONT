b00b82b53f147d86dcb4774daf7b651a1ed30411
abouamhster-cmyk <abouamhster@gmail.com>
2026-07-01 17:34:17 +0200

    Update index.ts

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,5 @@
 // 📁 src/components/ui/index.ts
- 
+
 export { Modal, ModalActions, ModalWithConfirm, ModalWithForm } from './Modal';
 export { InfoModal } from './InfoModal';
 export { InfoRow } from './InfoRow';
@@ -9,9 +9,8 @@ export { Logo } from './Logo';
 export { Illustration } from './Illustration';
 export { RoleBanner } from './RoleBanner';
 
+// ✅ Exports par défaut
 export { default as ModalDefault } from './Modal';
 export { default as InfoRowDefault } from './InfoRow';
 export { default as LoadingSpinnerDefault } from './LoadingSpinner';
 export { default as LogoDefault } from './Logo';
-
- 
